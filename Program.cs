87700e5c02132f03034b56eb5cb54498bc385217
anjaliraghu99 <anjaliraghu67@gmail.com>
2024-11-20 00:04:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+//Fibonacci series
+// 0 1,1,2,3,5,8
+
+var res = fibonacci(8);
+foreach (int item in res)
+{
+    Console.WriteLine(item);
+}
+
+
+static List<int> fibonacci(int n)
+{
+    int x = 0;
+    int y = 1;
+    int sum = 0;
+    List<int> list = new List<int>();
+
+    for (int i = 0; i < n; i++)
+    {
+        sum = x + y;
+        x = y;
+        y = sum;
+        list.Add(sum);
+
+    }
+    return list;
+
+}
+
+
+
+
+
+
+
+
+
